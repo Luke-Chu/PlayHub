bfab7751ddbc4290e6449a3528908333649793ca
Luke <Luke-Chu@outlook.com>
2025-11-19 17:52:49 +0800

    fix：判断是不是自己的锁再删除

--- src/main/java/com/luke/playhub/utils/SimpleRedisLock.java
+++ src/main/java/com/luke/playhub/utils/SimpleRedisLock.java
@@ -2,6 +2,8 @@ package com.luke.playhub.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.UUID;
+
 /**
  * @author Luke
  * @since 2025/11/19 16:06
@@ -11,6 +13,7 @@ public class SimpleRedisLock implements ILock {
     private final String name;
     private final StringRedisTemplate redisTemplate;
     private final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID() + "-";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -21,12 +24,19 @@ public class SimpleRedisLock implements ILock {
     public boolean tryLock(long timeoutSec) {
         String threadId = String.valueOf(Thread.currentThread().threadId());
         Boolean success = redisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, java.util.concurrent.TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, ID_PREFIX + threadId, timeoutSec, java.util.concurrent.TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unlock() {
-        redisTemplate.delete(KEY_PREFIX + name);
+        // 获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().threadId();
+        // 获取锁中key的标识
+        String id = redisTemplate.opsForValue().get(KEY_PREFIX + name);
+        // 判断是不是自己线程的锁
+        if (threadId.equals(id)) {
+            redisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
