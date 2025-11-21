31df31ff2e283377cf357ed36f9b7cf197981325
Luke <Luke-Chu@outlook.com>
2025-11-21 17:52:15 +0800

    feat：新增优惠券时将优惠券库存放到Redis

--- src/main/java/com/luke/playhub/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/luke/playhub/service/impl/VoucherServiceImpl.java
@@ -4,7 +4,9 @@ import com.luke.playhub.entity.Voucher;
 import com.luke.playhub.mapper.VoucherMapper;
 import com.luke.playhub.service.VoucherService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Luke
@@ -14,10 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherServiceImpl implements VoucherService {
     private final VoucherMapper voucherMapper;
+    private final StringRedisTemplate stringRedisTemplate;
 
 
+    @Transactional
     @Override
     public void save(Voucher voucher) {
         voucherMapper.insert(voucher);
+        // 保存秒杀信息到Redis
+        stringRedisTemplate.opsForValue().set("stock:voucher:" + voucher.getId(), String.valueOf(voucher.getStock()));
     }
 }
